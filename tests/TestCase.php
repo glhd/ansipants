8a3ee1263c62ea99b7a31ade3aa3703a3b0e2a87
GitHub Action <action@github.com>
2024-04-12 20:53:54 +0000

    Template cleanup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Glhd\LaravelPackageTemplate\Tests;
+namespace Glhd\Ansipants\Tests;
 
-use Glhd\LaravelPackageTemplate\Support\LaravelPackageTemplateServiceProvider;
+use Glhd\Ansipants\Support\AnsipantsServiceProvider;
 use Illuminate\Container\Container;
 use Orchestra\Testbench\TestCase as Orchestra;
 
@@ -11,7 +11,7 @@ abstract class TestCase extends Orchestra
 	protected function getPackageProviders($app)
 	{
 		return [
-			LaravelPackageTemplateServiceProvider::class,
+			AnsipantsServiceProvider::class,
 		];
 	}
 	
