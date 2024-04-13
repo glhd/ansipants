eeceee9accde0deb5be8bd5d8a5ad7a1fb9cce89
Chris Morrell <inxilpro@users.noreply.github.com>
2024-04-12 23:57:56 -0400

    moar emoji

--- tests/Unit/AnsiStringTest.php
+++ tests/Unit/AnsiStringTest.php
@@ -9,7 +9,7 @@ class AnsiStringTest extends TestCase
 {
 	public function test_it_can_be_instantiated_from_a_string_with_ansi_sequences(): void
 	{
-		$original = "\e[1mHello \e[3mwo\e[0mrld";
+		$original = "\e[1mHello💥 \e[3mwo\e[0mrld 🥸";
 		$parsed = new AnsiString($original);
 		
 		$this->assertEquals($original, (string) $parsed);
