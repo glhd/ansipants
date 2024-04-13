1c6e057424d1915ee619bf78255c85c01bb26557
Chris Morrell <inxilpro@users.noreply.github.com>
2024-04-13 00:30:12 -0400

    Code style

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -2,7 +2,7 @@
 
 use Glhd\AnsiPants\AnsiString;
 
-if (!function_exists('ansi')) {
+if (! function_exists('ansi')) {
 	function ansi(string $value): AnsiString
 	{
 		return new AnsiString($value);
